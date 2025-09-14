c45c75e53889e0f54685bf79b8059d4325ab5547
dazedniteman <73503406+dazedniteman@users.noreply.github.com>
2025-09-13 22:59:34 -0400

    Update index.js

--- src/state/slices/index.js
+++ src/state/slices/index.js
@@ -1,9 +1,11 @@
+// src/state/slices/index.js - Updated with enhanced slices
+
 import * as favorites from './favorites'
-import * as filters from './filters'
+import * as filters from './filters' // This now includes view mode and table sorting
 import * as message from './message'
 import * as playlist from './playlist'
 import * as settings from './settings'
-import * as sync from './sync'
+import * as sync from './sync' // This now includes affinity and Last.fm data
 import * as update from './update'
 
 const slices = [favorites, filters, message, playlist, settings, sync, update]
