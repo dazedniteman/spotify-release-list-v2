192533b911efce107b4366d4446a66dc0d9926c7
dazedniteman <73503406+dazedniteman@users.noreply.github.com>
2025-09-16 10:52:30 -0400

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,14 @@
 // src/components/index.js - Add the new components
 
-// ... your existing exports
+// Export existing components
+export { default as App } from './App'
+export { default as Auth } from './Auth'
+export { default as Routes } from './Routes'
+
+// Export releases components
+export { Releases } from './releases'
+
+// Export new enhanced components
 export { default as TableView } from './TableView'
 export { default as ViewModeToggle } from './ViewModeToggle'  
 export { default as SyncProgress } from './SyncProgress'
-export { default as Releases } from './Releases' // if you're updating this
